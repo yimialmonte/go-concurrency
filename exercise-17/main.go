be53e3b7d6b8e58be7f58b1cb9d01b6c5bb38e2a
yimialmonte <yalmonte@bancolombia.com.co>
2021-12-17 17:56:22 -0400

    exercise race condition

---
+++ exercise-17/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+//TODO: identify the data race
+// fix the issue.
+
+func main() {
+	start := time.Now()
+	var t *time.Timer
+	ch := make(chan bool)
+
+	t = time.AfterFunc(randomDuration(), func() {
+		fmt.Println(time.Now().Sub(start))
+		ch <- true
+	})
+
+	for time.Since(start) < 5*time.Second {
+		<-ch
+		t.Reset(randomDuration())
+	}
+
+	time.Sleep(5 * time.Second)
+}
+
+func randomDuration() time.Duration {
+	return time.Duration(rand.Int63n(1e9))
+}
+
+//----------------------------------------------------
+// (main goroutine) -> t <- (time.AfterFunc goroutine)
+//----------------------------------------------------
+// (working condition)
+// main goroutine..
+// t = time.AfterFunc()  // returns a timer..
+
+// AfterFunc goroutine
+// t.Reset()        // timer reset
+//----------------------------------------------------
+// (race condition- random duration is very small)
+// AfterFunc goroutine
+// t.Reset() // t = nil
+
+// main goroutine..
+// t = time.AfterFunc()
+//----------------------------------------------------
